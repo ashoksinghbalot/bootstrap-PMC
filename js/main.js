b1ec963a3bc356274390708d6306b3a0215fff04
ashoksinghbalot <as7416063@gmail.com>
2022-11-19 18:35:32 +0530

    update html and css code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function scrollbar(){
+function topscroll(){
     window.scrollTo(0,0)
   }
 
   window.onscroll = function(){
+    console.log(window.scrollY)
     if(window.scrollY>800){
       document.querySelector(".arrow").style.display="block";
-
     }
     else{
       document.querySelector(".arrow").style.display="none";
@@ -14,24 +14,18 @@ function scrollbar(){
     if (window.scrollY>400){
       document.getElementById("navbarevent").style.backgroundColor="white";
       document.getElementById("navbarevent").style.boxShadow="rgba(0,0,0,5) 0px 3px 8px";
-      
       let items=document.querySelectorAll(".menuitem li a");
         for(let item of items){
           item.style.color="black";
-
         }
     }
-    else if (window.scrollY<300){
+    else if (window.scrollY<400){
       document.getElementById("navbarevent").style.backgroundColor="black";
       document.getElementById("navbarevent").style.boxShadow="";
-
       let items=document.querySelectorAll(".menuitem li a");
-      
       for (let item of items){
-        item.setAttribute("style", " ")
+        item.style.color="";
       }
-    }
-
-
-  
+    }  
   }
+  
